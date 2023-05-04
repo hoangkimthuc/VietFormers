a04937319e2787a0170bf240e0a30c989543be71
ThucHK <hoangkimthuc.ryan@gmail.com>
2023-05-04 10:25:49 +0700

    add code and requirements.txt

--- model.py
+++ model.py
@@ -1,7 +1,29 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# Write the self-attention model here
+
+
+class FFN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers, dropout_p=0.1):
+        super(FFN, self).__init__()
+        layers = []
+        layers.append(nn.Linear(input_size, hidden_size))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(dropout_p))
+
+        for _ in range(num_layers - 2):
+            layers.append(nn.Linear(hidden_size, hidden_size))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout_p))
+            
+        layers.append(nn.Linear(hidden_size, output_size))
+
+        self.ffn = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.ffn(x)
+
+# Single self-attention head
 class AttentionHead(nn.Module):
     def __init__(self, Q, K, V):
         super(AttentionHead, self).__init__()
@@ -19,20 +41,44 @@ class AttentionHead(nn.Module):
         z = torch.matmul(q_k_softmax, v)
         return z
 
-class MultiAttention(nn.Module):
-    def __init__(self, Q, K, V, heads):
-        super(MultiAttention, self).__init__()
+class Encoder(nn.Module):
+    def __init__(self, Q, K, V, heads, hidden_size=512, num_layers=4, dropout_p=0.1):
+        super(Encoder, self).__init__()
+        self.ffn = FFN(V, hidden_size, V, num_layers, dropout_p)
         self.heads = heads
         self.attention_heads = nn.ModuleList()
         for _ in range(heads):
-            self.attention_heads.append(AttentionHead(Q, K, V))
+            self.attention_heads.append(AttentionHead(Q, K, V))   #create a list of attention heads (multi-head attention)
         self.linear = nn.Linear(heads*V, V)
+        self.layer_norm1 = nn.LayerNorm(V)
+        self.layer_norm2 = nn.LayerNorm(V)
     
     def forward(self, x):
         z = []
         for i in range(self.heads):
-            z.append(self.attention_heads[i](x))
+            z.append(self.attention_heads[i](x)) #apply attention heads
         z = torch.cat(z, dim=1)
         z = self.linear(z)
+        z = self.layer_norm1(x + z) #residual connection + layer normalization
+        z1 = self.ffn(z)
+        z = self.layer_norm2(z + z1)
         return z
+
+class MultiEncoder(nn.Module):
+    def __init__(self, Q, K, V, heads, num_encoder, hidden_size=512, num_layers=4, dropout_p=0.1):
+        super(MultiEncoder, self).__init__()
+        self.multiencoder = nn.ModuleList()
+        self.num_encoder = num_encoder
+        for _ in range(num_encoder):
+            self.multiencoder.append(Encoder(Q, K, V, heads, hidden_size, num_layers, dropout_p))
+        self.pos_encoding = None
+        self.attention_mask = None
+
+    def forward(self, x):
+        z = x
+        for i in range(self.num_encoder):
+            z = self.multiencoder[i](z)
+        return z
+
+
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,13 @@
 import torch
-from model import AttentionHead, MultiAttention
+from model import AttentionHead, Encoder, FFN, MultiEncoder
 
-# Test the above class
+def test_ffn():
+    input_size, hidden_size, output_size, num_layers = 10, 20, 10, 4
+    x = torch.randn(2, 10)
+    ffn = FFN(input_size, hidden_size, output_size, num_layers)
+    z = ffn(x)
+    assert z.shape == (2, 10)
+
 def test_attention_head():
     Q, K, V = 10, 10, 10
     x = torch.randn(2, 10)
@@ -9,9 +15,17 @@ def test_attention_head():
     z = attention_head(x)
     assert z.shape == (2, 10)
 
-def test_multi_attention():
+def test_encoder():
     Q, K, V, heads = 10, 10, 10, 5
     x = torch.randn(2, 10)
-    multi_attention = MultiAttention(Q, K, V, heads)
+    multi_attention = Encoder(Q, K, V, heads)
     z = multi_attention(x)
     assert z.shape == (2, 10)
+
+def test_multi_encoder():
+    Q, K, V, heads, num_encoder = 10, 10, 10, 5, 3
+    x = torch.randn(2, 10)
+    multi_encoder = MultiEncoder(Q, K, V, heads, num_encoder)
+    z = multi_encoder(x)
+    assert z.shape == (2, 10)
+   
