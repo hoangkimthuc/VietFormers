d112450dd69d7d8ee87aace6cfbd62d0aa192b00
thuchk <hoangkimthuc.ryan@gmail.com>
2023-07-07 01:53:24 +0000

    add wandb for logging and visualization

--- train_BERT.py
+++ train_BERT.py
@@ -1,14 +1,14 @@
 from models.BERT import BERT
 import math
-import os
-from tempfile import TemporaryDirectory
-from typing import Tuple
-from omegaconf import DictConfig, OmegaConf
+from typing import Tuple, Union
 import hydra
+import wandb
+import datetime
 
 import torch
 from torch import nn, Tensor
 from torch.utils.data import dataset
+from torch.utils.data.dataset import IterableDataset
 from torchtext.datasets import WikiText2
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
@@ -21,7 +21,7 @@ import time
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def data_process(raw_text_iter: dataset.IterableDataset, vocab, tokenizer) -> Tensor:
+def data_process(raw_text_iter: IterableDataset, vocab, tokenizer) -> Tensor:
     """Converts raw text into a flat Tensor."""
     data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
     return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
@@ -57,12 +57,15 @@ def get_batch(source: Tensor, i: int, sq_len: int) -> Tuple[Tensor, Tensor]:
     target = rearrange(source[i+1:i+1+sq_len], 'seq batch -> batch seq').reshape(-1)
     return data, target
 
-def train(model: nn.Module, train_data, val_data, num_epochs, criterion, lr, optimizer, scheduler, ntokens, sq_len) -> None:
+def train(model: nn.Module, train_data: Tensor, val_data: Tensor, 
+          num_epochs:int, criterion, lr:Union[float, int], 
+          optimizer, scheduler, ntokens:int, sq_len:int,
+          log_interval: int, wandb_enabled: bool) -> None:
     for epoch in range(1, num_epochs + 1):
         epoch_start_time = time.time()
         model.train()  # turn on train mode
         total_loss = 0.
-        log_interval = 200
+        log_interval = log_interval
         start_time = time.time()
 
         num_batches = len(train_data) // sq_len
@@ -89,8 +92,18 @@ def train(model: nn.Module, train_data, val_data, num_epochs, criterion, lr, opt
                     f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
                 total_loss = 0
                 start_time = time.time()
+                if wandb_enabled: 
+                    wandb.log({"loss": loss}, step = batch)
+            
+                
+            
         val_loss = evaluate(model, val_data,criterion,sq_len,ntokens)
         val_ppl = math.exp(val_loss)
+        
+        if wandb_enabled:
+            wandb.log({"val_loss": val_loss, "epoch": epoch})
+            wandb.log({"val_ppl": val_ppl, "epoch": epoch})
+
         elapsed = time.time() - epoch_start_time
         print('-' * 89)
         print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
@@ -126,32 +139,55 @@ def main(cfg) -> None:
     train_data = data_process(train_iter, vocab, tokenizer)
     val_data = data_process(val_iter, vocab, tokenizer)
     test_data = data_process(test_iter, vocab, tokenizer)
-
-    train_batch_size = cfg.training.train_batch_size
-    eval_batch_size = cfg.training.eval_batch_size
     
     ntokens = len(vocab)  # size of vocabulary
-    train_data = batchify(train_data, train_batch_size)  # shape ``[seq_len, batch_size]``
-    val_data = batchify(val_data, eval_batch_size)
-    test_data = batchify(test_data, eval_batch_size)
-
-    emb_dim = cfg.model.emb_dim    
-    num_attention_heads = cfg.model.num_attention_heads
-    num_encoder_blocks = cfg.model.num_encoder_blocks
-    dropout_p = cfg.model.dropout_p
+    train_data = batchify(train_data, cfg.training.train_batch_size)  # shape ``[seq_len, batch_size]``
+    val_data = batchify(val_data, cfg.training.eval_batch_size)
+    test_data = batchify(test_data, cfg.training.eval_batch_size)
     
-    sq_len = cfg.training.sq_len 
-    model = BERT(emb_dim=emb_dim, vocab_size=ntokens, num_attention_heads=num_attention_heads, num_encoder_blocks=num_encoder_blocks, max_seq_len=sq_len, dropout_p=dropout_p)
+    
+    model = BERT(emb_dim=cfg.model.emb_dim, 
+                 vocab_size=ntokens, 
+                 num_attention_heads=cfg.model.num_attention_heads, 
+                 num_encoder_blocks=cfg.model.num_encoder_blocks, 
+                 dropout_p=cfg.model.dropout_p)
     model = model.to(device)
 
     criterion = nn.CrossEntropyLoss()
-    lr = cfg.training.lr
 
-    num_epochs = cfg.training.num_epochs # The number of epochs
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+     # The number of epochs
+    optimizer = torch.optim.SGD(model.parameters(), lr=cfg.training.lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+    #Add wandb for logging training loss and validation loss
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-    train(model, train_data, val_data, num_epochs, criterion, lr, optimizer, scheduler, ntokens, sq_len)
+    if cfg.wandb.is_enabled:
+        wandb.init(
+        project=cfg.wandb.project, 
+        name=f"{cfg.wandb.run_name}_{current_datetime}", 
+        )
+        wandb.define_metric("epoch")
+        wandb.define_metric("val_loss", step_metric="epoch")
+        wandb.define_metric("val_ppl", step_metric="epoch")
+        if cfg.wandb.watch_model:
+            wandb.watch(model, log="gradients", 
+                        criterion=criterion, 
+                        log_freq=cfg.wandb.log_freq, 
+                        log_graph=True)
+    
+    train(model=model,
+          train_data=train_data,
+          val_data=val_data,
+          num_epochs=cfg.training.num_epochs,
+          criterion=criterion,
+          lr=cfg.training.lr, 
+          optimizer=optimizer, 
+          scheduler=scheduler,
+          ntokens=ntokens, 
+          sq_len=cfg.training.sq_len ,
+          log_interval=cfg.training.log_interval,
+          wandb_enabled=cfg.wandb.is_enabled)
 
 
 if __name__ == "__main__":
