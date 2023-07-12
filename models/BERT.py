1ac182b457aff5528edf6ffd4705dfc12726afc3
thuchk <hoangkimthuc.ryan@gmail.com>
2023-07-12 02:49:36 +0000

    remove default args for each block, use different drop prob for each layer

--- models/BERT.py
+++ models/BERT.py
@@ -3,25 +3,27 @@ from torch import nn
 from torch import Tensor
 
 class BERT(nn.Module):
-    def __init__(self, emb_dim:int, vocab_size:int, max_seq_len:int=512, num_attention_heads:int=5, num_encoder_blocks:int=3, hidden_size:int=512, num_layers:int=4, dropout_p:float=0.1):
+    def __init__(self, emb_dim:int, num_attention_heads:int, 
+                 hidden_size:int, num_layers:int, ffn_pdrop:float, 
+                 attn_pdrop:float, linear_proj_pdrop:float, 
+                 pos_emb_pdrop:float, emb_pdrop:float, bert_pdrop:float,
+                 vocab_size:int, max_seq_len:int, num_encoder_blocks:int):
         super(BERT, self).__init__()
-        self.encoder = Encoder(emb_dim=emb_dim,  
-                               vocab_size=vocab_size,
-                               max_seq_len=max_seq_len,
-                               num_attention_heads=num_attention_heads, 
-                               num_encoder_blocks=num_encoder_blocks,
-                               hidden_size=hidden_size,
-                               num_layers=num_layers,
-                               dropout_p=dropout_p)
-
+        self.encoder = Encoder(emb_dim=emb_dim, num_attention_heads=num_attention_heads,
+                            hidden_size=hidden_size, num_layers=num_layers,
+                            ffn_pdrop=ffn_pdrop, attn_pdrop=attn_pdrop,
+                            linear_proj_pdrop=linear_proj_pdrop,
+                            pos_emb_pdrop= pos_emb_pdrop, emb_pdrop=emb_pdrop,
+                            vocab_size=vocab_size, max_seq_len=max_seq_len, 
+                            num_encoder_blocks=num_encoder_blocks)
         self.linear = nn.Linear(emb_dim, vocab_size)
-        self.dropout = nn.Dropout(dropout_p)
+        self.bert_pdrop = nn.Dropout(bert_pdrop)
     
     def forward(self, x: Tensor):
         """
         x: (batch_size, sequence_length)
-        """   
+        """
         x = self.encoder(x)
-        x = self.linear(x)
-        x = self.dropout(x)
+        x = self.linear(x) 
+        x = self.bert_pdrop(x)
         return x
--- models/vanilla_transformers.py
+++ models/vanilla_transformers.py
@@ -5,22 +5,19 @@ import math
 
 from torch import Tensor
 from einops import rearrange
-from hydra import initialize, compose
-import omegaconf
-
 
 class FFN(nn.Module):
-    def __init__(self, input_size:int, hidden_size:int, output_size:int, num_layers:int, dropout_p:float=0.1):
+    def __init__(self, input_size:int, hidden_size:int, output_size:int, num_layers:int, ffn_pdrop:float):
         super(FFN, self).__init__()
         layers = []
         layers.append(nn.Linear(input_size, hidden_size))
         layers.append(nn.ReLU())
-        layers.append(nn.Dropout(dropout_p))
+        layers.append(nn.Dropout(ffn_pdrop))
 
         for _ in range(num_layers - 2):
             layers.append(nn.Linear(hidden_size, hidden_size))
             layers.append(nn.ReLU())
-            layers.append(nn.Dropout(dropout_p))
+            layers.append(nn.Dropout(ffn_pdrop))
             
         layers.append(nn.Linear(hidden_size, output_size))
 
@@ -29,39 +26,25 @@ class FFN(nn.Module):
     def forward(self, x:Tensor):
         return self.ffn(x)
 
-class MultiHeadAttention(nn.Module):
-    """
-    A vanilla multi-head masked self-attention layer with a projection at the end.
-    It is possible to use torch.nn.MultiheadAttention here but I am including an
-    explicit implementation here to show that there is nothing too scary here.
-    """
-    @staticmethod
-    def get_default_cfg():
-        cfg = omegaconf.OmegaConf.load("conf/BERT.yaml")
-        return cfg
-
-    def __init__(self):
+class MultiHeadAttention(nn.Module):  
+    def __init__(self, emb_dim:int, num_attention_heads:int, attn_pdrop:float, linear_proj_pdrop:float):
         super().__init__()
 
-        config = self.get_default_cfg()
-        # assert config.model.emb_dim % config.model.num_attention_heads == 0
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.model.emb_dim, 3 * config.model.num_attention_heads*config.model.emb_dim)
+        self.c_attn = nn.Linear(emb_dim, 3 * num_attention_heads*emb_dim, bias=False)
         # output projection
-        self.c_proj = nn.Linear(config.model.num_attention_heads*config.model.emb_dim, config.model.emb_dim)
+        self.c_proj = nn.Linear(num_attention_heads*emb_dim, emb_dim)
         # regularization
-        self.attn_dropout = nn.Dropout(config.model.attn_pdrop)
-        self.resid_dropout = nn.Dropout(config.model.resid_pdrop)
-        
-        self.n_head = config.model.num_attention_heads
-        self.n_embd = config.model.emb_dim
+        self.attn_dropout = nn.Dropout(attn_pdrop)
+        self.linear_proj_dropout = nn.Dropout(linear_proj_pdrop)        
+        self.n_head = num_attention_heads
+        self.n_embd = emb_dim
 
     def forward(self, x):
-        config = self.get_default_cfg()
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.c_attn(x).split(config.model.num_attention_heads*self.n_embd, dim=2)
+        q, k ,v  = self.c_attn(x).split(self.n_head*self.n_embd, dim=2)
         k = k.view(B, T, self.n_head, C ).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C ).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C ).transpose(1, 2) # (B, nh, T, hs)
@@ -71,18 +54,18 @@ class MultiHeadAttention(nn.Module):
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, config.model.num_attention_heads*C) # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, T, self.n_head*C) # re-assemble all head outputs side by side
 
         # output projection
-        y = self.resid_dropout(self.c_proj(y))
+        y = self.linear_proj_dropout(self.c_proj(y))
         return y
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, emb_dim:int, num_attention_heads:int, hidden_size:int=512, num_layers:int=4, dropout_p=0.1):
+    def __init__(self, emb_dim:int, num_attention_heads:int, hidden_size:int, num_layers:int, ffn_pdrop:float, attn_pdrop:float, linear_proj_pdrop:float):
         super(EncoderBlock, self).__init__()
-        self.ffn = FFN(emb_dim, hidden_size, emb_dim, num_layers, dropout_p)        
-        self.multi_head_attention = MultiHeadAttention()
+        self.ffn = FFN(emb_dim, hidden_size, emb_dim, num_layers, ffn_pdrop)        
+        self.multi_head_attention = MultiHeadAttention(emb_dim=emb_dim, num_attention_heads=num_attention_heads, attn_pdrop=attn_pdrop, linear_proj_pdrop=linear_proj_pdrop)
         self.layer_norm1 = nn.LayerNorm(emb_dim)
         self.layer_norm2 = nn.LayerNorm(emb_dim)
     
@@ -94,16 +77,23 @@ class EncoderBlock(nn.Module):
         return z
     
 class Encoder(nn.Module):
-    def __init__(self, emb_dim:int, vocab_size:int, num_attention_heads=5, num_encoder_blocks=3, max_seq_len=512, hidden_size=512, num_layers=4, dropout_p=0.1):
+    def __init__(self, emb_dim:int, num_attention_heads:int, 
+                 hidden_size:int, num_layers:int, ffn_pdrop:float, 
+                 attn_pdrop:float, linear_proj_pdrop:float, 
+                 pos_emb_pdrop:float, emb_pdrop:float,
+                 vocab_size:int, max_seq_len:int, num_encoder_blocks:int):
         super(Encoder, self).__init__()
         self.encoder = nn.Sequential()
-        self.embedding = nn.Sequential(nn.Embedding(vocab_size, emb_dim), nn.Dropout(dropout_p))
-        self.pos_encoding = PositionalEncoding(emb_dim, dropout_p, max_seq_len)
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.emb_drop = nn.Dropout(emb_pdrop)
+        self.pos_encoding = PositionalEncoding(emb_dim, pos_emb_pdrop, max_seq_len)
         for _ in range(num_encoder_blocks):
-            self.encoder.append(EncoderBlock(emb_dim, num_attention_heads, hidden_size, num_layers, dropout_p))
-
+            self.encoder.append(EncoderBlock(emb_dim=emb_dim, num_attention_heads=num_attention_heads, 
+                                   hidden_size=hidden_size, num_layers=num_layers, 
+                                   ffn_pdrop=ffn_pdrop, attn_pdrop=attn_pdrop, 
+                                   linear_proj_pdrop=linear_proj_pdrop))
     def forward(self, x:Tensor):
-        x = self.embedding(x)
+        x = self.emb_drop(self.embedding(x))
         x = rearrange(x, 'b s e -> s b e') # rearrange to fit positional encoding
         x = self.pos_encoding(x)
         x = rearrange(x, 's b e -> b s e')
@@ -112,13 +102,12 @@ class Encoder(nn.Module):
 
     
 class PositionalEncoding(nn.Module):
-
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 512):
+    def __init__(self, emb_dim: int, pos_emb_pdrop: float, max_seq_len: int):
         super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
+        self.dropout = nn.Dropout(p=pos_emb_pdrop)
+        position = torch.arange(max_seq_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, emb_dim, 2) * (-math.log(10000.0) / emb_dim))
+        pe = torch.zeros(max_seq_len, 1, emb_dim)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
